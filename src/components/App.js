86e4edf0a10a728334db86c029f9fe328de707b2
Dmitry Grinchenko <ra1d3r@i.ua>
2021-02-25 20:25:44 +0200

    savepoint

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Section from "./section/Section";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        return (good + neutral + bad)
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        return this.countTotalFeedback() !== 0 ? (good / this.countTotalFeedback()) * 100 : 0
+    };
+
+    leaveFeedback = (event) => {
+        const name = event.target.name
+        this.setState((prevState) => {
+            return {
+                [name]: prevState[name] + 1
+            }
+        })
+    }
+
+    render() {
+        const { good, neutral, bad } = this.state;
+
+        return (
+          <>
+            <Section title="Please leave feedback">               
+                <button name="good" type="button" onClick={this.leaveFeedback}>Good</button>
+                <button name="neutral" type="button" onClick={this.leaveFeedback}>Neutral</button>
+                <button name="bad" type="button" onClick={this.leaveFeedback}>Bad</button>
+            </Section>
+
+             <Section title="Statistics">
+                <ul>
+                    <li><p>Good : {good}</p></li>
+                    <li><p>Neuntral : {neutral}</p></li>
+                    <li><p>Bad : {bad}</p></li>
+                    <li><p>Total : {this.countTotalFeedback()}</p></li>
+                    <li><p>Positive feedback : {this.countPositiveFeedbackPercentage().toFixed(0)}%</p></li>
+                </ul>
+            </Section>
+            </>
+        )
+    }
 }
-
 export default App;
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+const Statistics = () => { }
+
+
+export default Statistics
